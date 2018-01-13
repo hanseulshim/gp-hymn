6a7b9f40b47422a4ca8b10b203b9dbd4ae05e916
Hanseul Shim <hanseulshim@Hanseuls-MacBook-Pro.local>
2018-01-12 20:53:33 -0500

    add cors and hook up db to react

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import compression from 'compression';
 import helmet from 'helmet';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import Hymn from './db/Models/Hymn';
 
@@ -15,6 +16,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
+app.use(cors());
 
 const url = 'mongodb://localhost:27017/hymns';
 mongoose.connect(url, { useMongoClient: true });
