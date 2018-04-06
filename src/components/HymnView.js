b8b2097db9b7ec0029101a010180f69c933648a1
hanseulshim <hanseulshim@gmail.com>
2018-04-06 16:37:01 -0400

    hymnview displays hymn and allows back to home

--- src/components/HymnView.js
+++ src/components/HymnView.js
@@ -1,54 +1,84 @@
 import React from 'react';
-import shortid from 'shortid';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link, Redirect } from 'react-router-dom';
+import * as Actions from '../actions';
 
-const container = {
-  width: '70%',
-  padding: '20px',
-  fontSize: '20px',
-  background: '#e9ece5'
-}
 
-const chordSheet = {
-  width: '500px',
-  whiteSpace: 'pre-wrap',
-  textAlign: 'left',
-  fontSize: '16px',
-  padding: '10px',
-  margin: 'auto'
-}
-
-const createScan = (props) => {
-  // const scan = props.selectedHymn.scan.slice();
-  const scan = ['v1', 'chorus', 'v2', 'v3', 'v4', 'v5'];
-  return scan.map(v => props.selectedHymn[v] === '' ? null : (
-    <div key={shortid.generate()}>
-      <h4>{v}:</h4>
-      <p>{props.selectedHymn[v]}</p>
-    </div>
-  ));
-};
-
-const HymnView = props => (
-<div style={container}>
-    {
-      props.selectedHymn.name === 'Select a Hymn' ?
-        <h2>No hymn selected</h2> :
-        <div style={chordSheet}>
-          <h2>Name:</h2>
-          <p>{props.selectedHymn.name}</p>
-          <h2>Original Key:</h2>
-          <p>{props.selectedHymn.originalKey}</p>
-          <h2>Scan:</h2>
-          {createScan(props)}
-        </div>
-    }
-  </div>
-);
-
-HymnView.defaultProps = {
-  selectedHymn: {
-    name: 'Select a Hymn',
+const styles = {
+  container: {
+    height: '100%',
+    background: '#d5e1df',
+  },
+  header: {
+    boxShadow: '0px 0px 10px 4px rgba(0,0,0,0.75)',
+    position: 'fixed',
+    width: '100%',
+    height: 125,
+    boxSizing: 'border-box',
+    padding: 15,
+    marginTop: -125,
+    background: '#b5e7a0',
+    zIndex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+  },
+  title: {
+    fontWeight: 500,
+    fontSize: 25,
+    textAlign: 'center',
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  lyricsContainer: {
+    marginTop: 125,
+    display: 'flex',
+    justifyContent: 'center',
+    background: '#d5e1df',
+  },
+  lyrics: {
+    padding: 30,
+    whiteSpace: 'pre-line',
   },
 };
 
-export default HymnView;
+const HymnView = (props) => {
+  const { title, lyrics } = props;
+  return title === '' ? (
+    <Redirect
+      to={{
+        pathname: '/',
+      }}
+    />
+  ) : (
+    <div style={styles.container}>
+      <div style={styles.header}>
+        <div style={styles.title}>
+          {title}
+        </div>
+        <Link to="/">{'<< Back'}</Link>
+      </div>
+      <div style={styles.lyricsContainer}>
+        <p style={styles.lyrics}>{lyrics}</p>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  title: state.hymnReducer.title,
+  lyrics: state.hymnReducer.lyrics,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(Actions, dispatch);
+
+HymnView.propTypes = {
+  title: PropTypes.string.isRequired,
+  lyrics: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HymnView);
